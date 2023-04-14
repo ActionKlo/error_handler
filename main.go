510909aa00ba86fb589e737819112edd5217ddb8
ActionKlo <actionclaw@gmail.com>
2023-04-14 18:23:25 +0200

    use channel for error message

--- main.go
+++ main.go
@@ -5,12 +5,25 @@ import "fmt"
 func main() {
 	categories := []string{"Category2", "Category4", "NoCategory"}
 
-	for _, cat := range categories {
-		total, err := Products.TotalPrice(cat)
-		if err == nil {
-			fmt.Println(cat, "Total:", ToCurrenct(total))
+	/*
+		for _, cat := range categories {
+			total, err := Products.TotalPrice(cat)
+			if err == nil {
+				fmt.Println(cat, "Total:", ToCurrenct(total))
+			} else {
+				fmt.Println(cat, "(no such category)")
+			}
+		}
+	*/
+
+	channel := make(chan ChannelMessage, 10)
+
+	go Products.TotalPriceAsync(categories, channel)
+	for message := range channel {
+		if message.CategoryError == nil {
+			fmt.Println(message.Category, "Total:", ToCurrenct(message.Total))
 		} else {
-			fmt.Println(cat, "(no such category)")
+			fmt.Println(message.Category, "(no such category)")
 		}
 	}
 }
